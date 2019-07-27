ad2aceecbbb235fb6b5421e8b4de65c510c5ebe5
Leong Wei Kong <wkleong.2014@sis.smu.edu.sg>
2019-07-27 17:36:07 +0800

    Added picture to landing page

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { User } from './user.entity';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class LoginService {
   loginUrl = '';
   currentUser: User;
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   login(email: string, password: string): Observable<any> {
     return this.http.get(this.loginUrl);
@@ -15,6 +16,7 @@ export class LoginService {
 
   logout() {
     this.currentUser = null;
+    this.router.navigateByUrl('/home');
   }
 
   isLogin(): boolean {
